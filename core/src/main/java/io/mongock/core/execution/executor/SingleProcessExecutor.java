73b63173095343a949c38a2599d4e095409b839c
Antonio Perez Dieppa <aperezdieppa@gmail.com>
2023-05-09 17:12:56 +0100

    wip

--- core/src/main/java/io/mongock/core/execution/executor/SingleProcessExecutor.java
+++ core/src/main/java/io/mongock/core/execution/executor/SingleProcessExecutor.java
@@ -3,6 +3,7 @@ package io.mongock.core.execution.executor;
 import io.mongock.core.audit.writer.AuditWriter;
 import io.mongock.core.execution.navigator.StepNavigationOutput;
 import io.mongock.core.execution.navigator.StepNavigatorBuilder;
+import io.mongock.core.execution.summary.DefaultStepSummarizer;
 import io.mongock.core.execution.summary.ProcessSummary;
 import io.mongock.core.process.single.SingleExecutableProcess;
 import io.mongock.core.runtime.RuntimeHelper;
@@ -25,11 +26,13 @@ public class SingleProcessExecutor implements ProcessExecutor<SingleExecutablePr
                       RuntimeHelper runtimeHelper) throws ProcessExecutionException {
         ProcessSummary summary = new ProcessSummary();
         StepNavigatorBuilder navBuilder = StepNavigatorBuilder.reusableInstance();
+
         Stream<StepNavigationOutput> taskStepStream = executableProcess.getTasks()
                 .stream()
                 .map(task -> navBuilder
                         .setAuditWriter(auditWriter)
                         .setRuntimeHelper(runtimeHelper)
+                        .setSummarizer(new DefaultStepSummarizer())
                         .build()
                         .start(task, executionContext)
                 )
