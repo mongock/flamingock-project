d06e37eb13a4cf9f18f15abe7905ead1c37bd74f
Dieppa on the rocks <aperezdieppa@gmail.com>
2023-06-02 10:59:07 +0100

    mockito 4.11.0

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -24,7 +24,7 @@ dependencies {
     testImplementation("org.slf4j:slf4j-simple:2.0.6")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.2")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.2")
-    implementation("org.mockito:mockito-core:5.2.0")
+    implementation("org.mockito:mockito-core:4.11.0")
 }
 
 tasks.withType<Test>().configureEach {
