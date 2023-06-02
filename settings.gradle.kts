f72668427915a7391c2a62179e9d6f7f748f85eb
Antonio Perez Dieppa <aperezdieppa@gmail.com>
2023-06-02 12:42:14 +0100

    driver builder

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,5 +2,9 @@ rootProject.name = "flamingock-project"
 include("flamingock-core")
 include("flamingock-oss")
 include("flamingock-cloud")
+
 include("mongodb-sync-v4-driver")
+project(":mongodb-sync-v4-driver").projectDir = file("driver/mongodb/mongodb-sync-v4-driver")
+
 include("mongodb-facade")
+project(":mongodb-facade").projectDir = file("driver/mongodb/mongodb-facade")
