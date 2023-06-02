25500b6e55373eb72d1d07aa6c4105c8b31a4c99
Antonio Perez Dieppa <aperezdieppa@gmail.com>
2023-06-02 12:45:11 +0100

    oss folder

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,10 +1,12 @@
 rootProject.name = "flamingock-project"
 include("flamingock-core")
-include("flamingock-oss")
 include("flamingock-cloud")
 
+include("flamingock-oss")
+project(":flamingock-oss").projectDir = file("oss/flamingock-oss")
+
 include("mongodb-sync-v4-driver")
-project(":mongodb-sync-v4-driver").projectDir = file("driver/mongodb/mongodb-sync-v4-driver")
+project(":mongodb-sync-v4-driver").projectDir = file("oss/mongodb/mongodb-sync-v4-driver")
 
 include("mongodb-facade")
-project(":mongodb-facade").projectDir = file("driver/mongodb/mongodb-facade")
+project(":mongodb-facade").projectDir = file("oss/mongodb/mongodb-facade")
