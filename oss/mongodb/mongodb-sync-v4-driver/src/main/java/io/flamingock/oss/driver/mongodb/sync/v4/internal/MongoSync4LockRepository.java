b20e88a87028f1861c1c39ff7791b2223f377fb9
Antonio Perez Dieppa <aperezdieppa@gmail.com>
2023-06-06 11:05:01 +0100

    added MongoDB syn4 repository

--- oss/mongodb/mongodb-sync-v4-driver/src/main/java/io/flamingock/oss/driver/mongodb/sync/v4/internal/MongoSync4LockRepository.java
+++ oss/mongodb/mongodb-sync-v4-driver/src/main/java/io/flamingock/oss/driver/mongodb/sync/v4/internal/MongoSync4LockRepository.java
@@ -1,9 +1,16 @@
 package io.flamingock.oss.driver.mongodb.sync.v4.internal;
 
+import com.mongodb.DuplicateKeyException;
+import com.mongodb.ErrorCategory;
+import com.mongodb.MongoWriteException;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.model.Filters;
+import com.mongodb.client.model.UpdateOptions;
+import com.mongodb.client.result.UpdateResult;
 import io.flamingock.oss.driver.common.mongodb.CollectionInitializator;
+import io.flamingock.oss.driver.common.mongodb.MongoDBAuditMapper;
+import io.flamingock.oss.driver.common.mongodb.MongoDBLockMapper;
 import io.flamingock.oss.driver.mongodb.sync.v4.internal.mongodb.MongoSync4CollectionWrapper;
 import io.flamingock.oss.driver.mongodb.sync.v4.internal.mongodb.MongoSync4DocumentWrapper;
 import io.flamingock.oss.internal.persistence.LockEntry;
@@ -22,6 +29,9 @@ import static io.flamingock.oss.internal.persistence.LockEntryField.*;
 public class MongoSync4LockRepository implements LockRepository {
 
     private static final Logger logger = LoggerFactory.getLogger(MongoSync4LockRepository.class);
+
+    private final MongoDBLockMapper<MongoSync4DocumentWrapper> mapper = new MongoDBLockMapper<>(() -> new MongoSync4DocumentWrapper(new Document()));
+
 
     private final MongoCollection<Document> collection;
 
@@ -44,27 +54,64 @@ public class MongoSync4LockRepository implements LockRepository {
 
     @Override
     public void upsert(LockEntry newLock) throws LockPersistenceException {
-
+        insertUpdate(newLock, false);
     }
 
     @Override
     public void updateOnlyIfSameOwner(LockEntry newLock) throws LockPersistenceException {
-
+        insertUpdate(newLock, true);
     }
 
     @Override
     public LockEntry findByKey(String lockKey) {
+        Document result = collection.find(new Document().append(KEY_FIELD, lockKey)).first();
+        if (result != null) {
+            return mapper.fromDocument(new MongoSync4DocumentWrapper(result));
+        }
         return null;
     }
 
     @Override
     public void removeByKeyAndOwner(String lockKey, String owner) {
-
+        collection.deleteMany(Filters.and(Filters.eq(KEY_FIELD, lockKey), Filters.eq(OWNER_FIELD, owner)));
     }
 
     @Override
     public void deleteAll() {
+        collection.deleteMany(new Document());
+    }
 
+
+    protected void insertUpdate(LockEntry newLock, boolean onlyIfSameOwner)  {
+        boolean lockHeld;
+        String debErrorDetail = "not db error";
+        Bson acquireLockQuery = getAcquireLockQuery(newLock.getKey(), newLock.getOwner(), onlyIfSameOwner);
+        Document lockDocument = mapper.toDocument(newLock).getDocument();
+        Document newLockDocumentSet = new Document().append("$set", lockDocument);
+        try {
+            UpdateResult result = collection.updateMany(acquireLockQuery, newLockDocumentSet, new UpdateOptions().upsert(!onlyIfSameOwner));
+            lockHeld = result.getModifiedCount() <= 0 && result.getUpsertedId() == null;
+
+        } catch (MongoWriteException ex) {
+            lockHeld = ex.getError().getCategory() == ErrorCategory.DUPLICATE_KEY;
+
+            if (!lockHeld) {
+                throw ex;
+            }
+            debErrorDetail = ex.getError().toString();
+
+        } catch (DuplicateKeyException ex) {
+            lockHeld = true;
+            debErrorDetail = ex.getMessage();
+        }
+
+        if (lockHeld) {
+            throw new LockPersistenceException(
+                    acquireLockQuery.toString(),
+                    newLockDocumentSet.toString(),
+                    debErrorDetail
+            );
+        }
     }
 
     protected Bson getAcquireLockQuery(String lockKey, String owner, boolean onlyIfSameOwner) {
