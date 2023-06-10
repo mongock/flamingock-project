e6d9a64fdea87cf05df4d9ca0907245910c6eeed
Antonio Perez Dieppa <aperezdieppa@gmail.com>
2023-06-10 18:08:05 +0100

    created a folder for core, cloud and oss

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,10 @@
 rootProject.name = "flamingock-project"
+
 include("flamingock-core")
+project(":flamingock-core").projectDir = file("core/flamingock-core")
+
 include("flamingock-cloud")
+project(":flamingock-cloud").projectDir = file("cloud/flamingock-cloud")
 
 include("flamingock-oss")
 project(":flamingock-oss").projectDir = file("oss/flamingock-oss")
