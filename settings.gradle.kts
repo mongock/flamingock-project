e86460a3204eac4574f550f84997c36ccb8789e8
Antonio Perez Dieppa <aperezdieppa@gmail.com>
2023-06-10 19:13:41 +0100

    added springContext

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,11 +1,27 @@
 rootProject.name = "flamingock-project"
 
+
+
+//////////////////////////////////////
+// CORE
+//////////////////////////////////////
 include("flamingock-core")
 project(":flamingock-core").projectDir = file("core/flamingock-core")
+project(":flamingock-core").name = "flamingock-core"
 
+include("flamingock-spring-core")
+project(":flamingock-spring-core").projectDir = file("core/flamingock-spring-core")
+project(":flamingock-spring-core").name = "flamingock-spring-core"
+
+//////////////////////////////////////
+// CORE
+//////////////////////////////////////
 include("flamingock-cloud")
 project(":flamingock-cloud").projectDir = file("cloud/flamingock-cloud")
 
+//////////////////////////////////////
+// OSS
+//////////////////////////////////////
 include("flamingock-oss")
 project(":flamingock-oss").projectDir = file("oss/flamingock-oss")
 
@@ -14,3 +30,5 @@ project(":mongodb-sync-v4-driver").projectDir = file("oss/mongodb/mongodb-sync-v
 
 include("mongodb-facade")
 project(":mongodb-facade").projectDir = file("oss/mongodb/mongodb-facade")
+
+
