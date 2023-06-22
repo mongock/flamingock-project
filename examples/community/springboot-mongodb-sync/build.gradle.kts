9e6ca4b64ae71e7129d547bcc24d92c483a41b89
Antonio Perez Dieppa <aperezdieppa@gmail.com>
2023-06-22 15:32:22 +0100

    added springboot application

--- examples/community/springboot-mongodb-sync/build.gradle.kts
+++ examples/community/springboot-mongodb-sync/build.gradle.kts
@@ -1,7 +1,24 @@
+plugins {
+    java
+    id("org.springframework.boot") version "2.7.12"
+    id("io.spring.dependency-management") version "1.1.0"
+}
+
+val mongodbVersion = "4.3.3"
 dependencies {
-//    implementation(project(":standalone-runner"))
+
+    implementation(project(":community:springboot-runner"))
     implementation(project(":community:mongodb:mongodb-sync-v4-driver"))
-    implementation("org.mongodb:mongodb-driver-sync:4.3.3")
+
+    implementation("org.slf4j:slf4j-simple:2.0.6")
+    implementation("org.mongodb:mongodb-driver-sync:$mongodbVersion")
+    implementation("org.mongodb:mongodb-driver-core:$mongodbVersion")
+    implementation("org.mongodb:bson:$mongodbVersion")
+    implementation("org.springframework.boot:spring-boot-starter-web")
 
     testImplementation("org.testcontainers:mongodb:1.18.3")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+
+
 }
+
--- examples/community/standalone-mongodb-sync/build.gradle.kts
+++ examples/community/standalone-mongodb-sync/build.gradle.kts
@@ -1,8 +1,11 @@
 
+val mongodbVersion = "4.3.3"
 dependencies {
     implementation(project(":community:standalone-runner"))
     implementation(project(":community:mongodb:mongodb-sync-v4-driver"))
-    implementation("org.mongodb:mongodb-driver-sync:4.3.3")
+    implementation("org.mongodb:mongodb-driver-sync:$mongodbVersion")
+    implementation("org.mongodb:mongodb-driver-core:$mongodbVersion")
+    implementation("org.mongodb:bson:$mongodbVersion")
 
     testImplementation("org.testcontainers:mongodb:1.18.3")
 }
