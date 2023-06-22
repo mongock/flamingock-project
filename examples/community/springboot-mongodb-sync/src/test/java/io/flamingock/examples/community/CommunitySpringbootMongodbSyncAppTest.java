2aea75704805173a8b7e889f00914b665fec899e
Antonio Perez Dieppa <aperezdieppa@gmail.com>
2023-06-22 20:27:34 +0100

    refactored springboot example test

--- examples/community/springboot-mongodb-sync/src/test/java/io/flamingock/examples/community/CommunitySpringbootMongodbSyncAppTest.java
+++ examples/community/springboot-mongodb-sync/src/test/java/io/flamingock/examples/community/CommunitySpringbootMongodbSyncAppTest.java
@@ -1,6 +1,9 @@
 package io.flamingock.examples.community;
 
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
 import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoDatabase;
 import io.flamingock.examples.community.changes.ACreateCollection;
 import io.flamingock.examples.community.changes.BInsertDocument;
@@ -9,6 +12,7 @@ import org.bson.Document;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Bean;
@@ -16,9 +20,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Primary;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.utility.DockerImageName;
 
-import static io.flamingock.examples.community.MongoDBTestHelper.mongoClient;
-import static io.flamingock.examples.community.MongoDBTestHelper.mongoDatabase;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
@@ -27,18 +31,30 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 @ExtendWith(SpringExtension.class)
 class CommunitySpringbootMongodbSyncAppTest {
 
+
+    public static final String DB_NAME = "test";
+    public static final MongoDBContainer mongoDBContainer = new MongoDBContainer(DockerImageName.parse("mongo:4.0.10"));
+
+    static {
+        mongoDBContainer.start();
+    }
+
+
     @Configuration
     static class TestConfiguration {
         @Bean
         @Primary
         public MongoClient mongoClient() {
-            return mongoClient;
+            return MongoClients.create(MongoClientSettings
+                    .builder()
+                    .applyConnectionString(new ConnectionString(mongoDBContainer.getConnectionString()))
+                    .build());
         }
 
         @Bean
         @Primary
-        public MongoDatabase mongoDatabase() {
-            return mongoDatabase;
+        public MongoDatabase mongoDatabase(MongoClient mongoClient) {
+            return mongoClient.getDatabase(DB_NAME);
         }
 
     }
@@ -46,14 +62,27 @@ class CommunitySpringbootMongodbSyncAppTest {
     public static final String AUDIT_LOG_COLLECTION = "mongockChangeLog";
     public static final String CLIENTS_COLLECTION = "clientCollection";
 
+    @Autowired
+    private MongoDatabase mongoDatabase;
 
     @BeforeEach
     public void setupEach() {
         mongoDatabase.getCollection(AUDIT_LOG_COLLECTION).deleteMany(new Document());
     }
 
+
     @Test
     void happyPath() {
+        assertEquals(ACreateCollection.class.getName(), ChangesTracker.changes.get(0));
+        assertEquals(BInsertDocument.class.getName(), ChangesTracker.changes.get(1));
+        assertEquals(CInsertAnotherDocument.class.getName(), ChangesTracker.changes.get(2));
+
+        //tear-down
+        mongoDatabase.getCollection(CLIENTS_COLLECTION).drop();
+    }
+
+    @Test
+    void happyPath2() {
         assertEquals(ACreateCollection.class.getName(), ChangesTracker.changes.get(0));
         assertEquals(BInsertDocument.class.getName(), ChangesTracker.changes.get(1));
         assertEquals(CInsertAnotherDocument.class.getName(), ChangesTracker.changes.get(2));
