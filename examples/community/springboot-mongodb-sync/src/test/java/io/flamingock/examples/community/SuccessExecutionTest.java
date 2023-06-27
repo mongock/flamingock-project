f4c7d15f07755615732ce410191ddf5358ffea5f
Antonio Perez Dieppa <aperezdieppa@gmail.com>
2023-06-27 20:21:17 +0100

    rebase(2)

--- examples/community/springboot-mongodb-sync/src/test/java/io/flamingock/examples/community/SuccessExecutionTest.java
+++ examples/community/springboot-mongodb-sync/src/test/java/io/flamingock/examples/community/SuccessExecutionTest.java
@@ -8,15 +8,9 @@ import com.mongodb.client.MongoDatabase;
 import io.flamingock.examples.community.changes.ACreateCollection;
 import io.flamingock.examples.community.changes.BInsertDocument;
 import io.flamingock.examples.community.changes.CInsertAnotherDocument;
-<<<<<<<< HEAD:examples/community/springboot-mongodb-sync/src/test/java/io/flamingock/examples/community/SuccessExecutionTest.java
 import io.flamingock.examples.community.events.FailureEventListener;
 import io.flamingock.examples.community.events.StartedEventListener;
 import io.flamingock.examples.community.events.SuccessEventListener;
-========
-import io.flamingock.examples.community.events.FailedFlamingockListener;
-import io.flamingock.examples.community.events.StartFlamingockListener;
-import io.flamingock.examples.community.events.SuccessFlamingockListener;
->>>>>>>> d117996 (added spring events to exampels):examples/community/springboot-mongodb-sync/src/test/java/io/flamingock/examples/community/SuccessTestTest.java
 import org.bson.Document;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -44,13 +38,8 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 @Testcontainers
 @AutoConfigureMockMvc
 @ExtendWith(SpringExtension.class)
-<<<<<<<< HEAD:examples/community/springboot-mongodb-sync/src/test/java/io/flamingock/examples/community/SuccessExecutionTest.java
 @Import({SuccessExecutionTest.TestConfiguration.class, CommunitySpringbootMongodbSyncApp.class})
 class SuccessExecutionTest {
-========
-@Import({SuccessTestTest.TestConfiguration.class, CommunitySpringbootMongodbSyncApp.class})
-class SuccessTestTest {
->>>>>>>> d117996 (added spring events to exampels):examples/community/springboot-mongodb-sync/src/test/java/io/flamingock/examples/community/SuccessTestTest.java
 
     @Container
     public static final MongoDBContainer mongoDBContainer = new MongoDBContainer(DockerImageName.parse("mongo:4.0.10"));
@@ -59,7 +48,6 @@ class SuccessTestTest {
     private StartedEventListener startedEventListener;
 
     @Autowired
-<<<<<<<< HEAD:examples/community/springboot-mongodb-sync/src/test/java/io/flamingock/examples/community/SuccessExecutionTest.java
     private SuccessEventListener successEventListener;
 
     @Autowired
@@ -81,39 +69,6 @@ class SuccessTestTest {
         assertTrue(startedEventListener.executed);
         assertTrue(successEventListener.executed);
         assertFalse(failureEventListener.executed);
-========
-    private StartFlamingockListener startFlamingockListener;
-
-    @Autowired
-    private SuccessFlamingockListener successFlamingockListener;
-
-    @Autowired
-    private FailedFlamingockListener failedFlamingockListener;
-
-
-
-    @BeforeEach
-    public void setupEach() {
-        mongoDatabase.getCollection("mongockChangeLog").deleteMany(new Document());
-        mongoDatabase.getCollection("clientCollection").drop();
-    }
-
-    @Test
-    @DisplayName("SHOULD execute all the changes WHEN executed IF happy path")
-    void happyPath() {
-        assertEquals(3, ChangesTracker.size());
-        assertEquals(ACreateCollection.class.getName(), ChangesTracker.get(0));
-        assertEquals(BInsertDocument.class.getName(), ChangesTracker.get(1));
-        assertEquals(CInsertAnotherDocument.class.getName(), ChangesTracker.get(2));
-    }
-
-    @Test
-    @DisplayName("SHOULD trigger start and success event WHEN executed IF happy path")
-    void events() {
-        assertTrue(startFlamingockListener.executed);
-        assertTrue(successFlamingockListener.executed);
-        assertFalse(failedFlamingockListener.executed);
->>>>>>>> d117996 (added spring events to exampels):examples/community/springboot-mongodb-sync/src/test/java/io/flamingock/examples/community/SuccessTestTest.java
     }
 
     @Configuration
