22d031cd23cbd0b7e8599d193be6c03152501d6f
Antonio Perez Dieppa <aperezdieppa@gmail.com>
2023-07-12 09:02:33 +0100

    refactor

--- core/flamingock-core/src/main/java/io/flamingock/core/core/process/single/SingleDefinitionProcess.java
+++ core/flamingock-core/src/main/java/io/flamingock/core/core/process/single/SingleDefinitionProcess.java
@@ -18,11 +18,11 @@ import java.util.stream.Collectors;
 
 public class SingleDefinitionProcess implements DefinitionProcess<SingleAuditProcessStatus, SingleExecutableProcess> {
 
-    private final List<String> scanPackages;
+    private final Collection<String> scanPackages;
     private Collection<TaskFilter> filters = new ArrayList<>();
 
     //We can pass here other sources, like yamls, etc.
-    public SingleDefinitionProcess(List<String> scanPackages) {
+    public SingleDefinitionProcess(Collection<String> scanPackages) {
         this.scanPackages = scanPackages;
     }
 
@@ -35,24 +35,25 @@ public class SingleDefinitionProcess implements DefinitionProcess<SingleAuditPro
      * Depending on the tasks inside the package or some field in the yaml, it returns a SingleLoadedProcess
      * or ParallelSingleLoadedProcess.
      * <br />
+     *
      * @return a sorted SingleLoadedProcess, non-sorted SingleLoadedProcess or a ParallelSingleLoadedProcess(non sorted),
      * depending on the task in the scanPackage,or some field in the yaml.
      */
     @Override
     public LoadedProcess<SingleAuditProcessStatus, SingleExecutableProcess> load() {
         //descriptors will potentially contain all the descriptors extracted form scanPackage, yaml, etc.
-        List<TaskDescriptor> descriptors = getDescriptorsFromScanPackage();
+        List<TaskDescriptor> descriptors = getFilteredDescriptorsFromScanPackages(scanPackages, filters);
 
         Optional<TaskDescriptor> orderedDescriptorOptional = descriptors
                 .stream()
                 .filter(descriptor -> descriptor instanceof SortedTaskDescriptor)
                 .findFirst();
 
-        if(descriptors.stream().allMatch(descriptor -> descriptor instanceof SortedTaskDescriptor)) {
+        if (descriptors.stream().allMatch(descriptor -> descriptor instanceof SortedTaskDescriptor)) {
             //if all descriptors are sorted, we return a sorted collection
             return new SingleLoadedProcess(descriptors.stream().sorted().collect(Collectors.toList()));
 
-        } else if(orderedDescriptorOptional.isPresent()) {
+        } else if (orderedDescriptorOptional.isPresent()) {
             //if at least one of them are sorted, but not all. An exception is thrown
             throw new IllegalArgumentException("Either all tasks are ordered or none is. Ordered task found: " + orderedDescriptorOptional.get().getId());
 
@@ -62,7 +63,10 @@ public class SingleDefinitionProcess implements DefinitionProcess<SingleAuditPro
         }
     }
 
-    private List<TaskDescriptor> getDescriptorsFromScanPackage() {
+    private static List<TaskDescriptor> getFilteredDescriptorsFromScanPackages(
+            Collection<String> scanPackages,
+            Collection<TaskFilter> filters) {
+
         return scanPackages.stream()
                 .map(ReflectionUtil::loadClassesFromPackage)
                 .flatMap(Collection::stream)
