ce2b5e23570578f9a34247bf9c2de5ce9610a3e9
Oliver Santana <oliver.sm@gmail.com>
2023-09-05 21:04:45 +0100

    WIP

--- examples/community/springboot-v3-mongodb-springdata-v4/build.gradle.kts
+++ examples/community/springboot-v3-mongodb-springdata-v4/build.gradle.kts
@@ -1,8 +1,8 @@
-/*plugins {
+plugins {
     id("java")
     id("org.springframework.boot") version "3.1.3"
     id("io.spring.dependency-management") version "1.1.3"
-}*/
+}
 
 dependencies {
 
@@ -10,14 +10,14 @@ dependencies {
     implementation(project(":community:mongodb:mongodb-springdata-v4-driver"))
     
     implementation("org.slf4j:slf4j-simple:2.0.6")
-    implementation("org.springframework.boot:spring-boot-starter-web:3.1.3")
-    implementation("org.springframework.boot:spring-boot-starter-data-mongodb:3.1.3")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
 
     testImplementation("org.testcontainers:mongodb:1.18.3")
 
     testImplementation("org.testcontainers:junit-jupiter:1.18.3")
 
-    testImplementation("org.springframework.boot:spring-boot-starter-test:3.1.3")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
 
     java {
         toolchain {
