75793ed42992cfe52f2e91499113f988b0ab02af
Antonio Perez Dieppa <aperezdieppa@gmail.com>
2023-09-14 18:36:20 +0100

    minor refactr

--- core/flamingock-core/src/main/java/io/flamingock/core/api/annotations/template/TransactionalFlamingockTemplate.java
+++ core/flamingock-core/src/main/java/io/flamingock/core/api/annotations/template/TransactionalFlamingockTemplate.java
@@ -1,6 +1,11 @@
 package io.flamingock.core.api.annotations.template;
 
 
+/**
+ * FlamingockTemplate extension Marker interface to indicate it must be executed within a transaction
+ *
+ * @see FlamingockTemplate
+ */
 public interface TransactionalFlamingockTemplate extends FlamingockTemplate {
 
 }
