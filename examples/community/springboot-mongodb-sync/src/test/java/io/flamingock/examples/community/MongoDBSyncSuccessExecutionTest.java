36bf87bd361b829d23f55b102b9b3619dfe86fdf
Antonio Perez Dieppa <aperezdieppa@gmail.com>
2023-09-21 23:17:30 +0100

    added integrationt tests

--- examples/community/springboot-mongodb-sync/src/test/java/io/flamingock/examples/community/MongoDBSyncSuccessExecutionTest.java
+++ examples/community/springboot-mongodb-sync/src/test/java/io/flamingock/examples/community/MongoDBSyncSuccessExecutionTest.java
@@ -1,20 +1,32 @@
 package io.flamingock.examples.community;
 
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
 import io.flamingock.examples.community.changes.ACreateCollection;
 import io.flamingock.examples.community.changes.BInsertDocument;
 import io.flamingock.examples.community.changes.CInsertAnotherDocument;
-import io.flamingock.examples.community.config.MongoInitializer;
+import io.flamingock.examples.community.changes.ProfileNotIncludedChange;
 import io.flamingock.examples.community.events.FailureEventListener;
 import io.flamingock.examples.community.events.StartedEventListener;
 import io.flamingock.examples.community.events.SuccessEventListener;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import org.springframework.test.context.ContextConfiguration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -22,9 +34,12 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 @SpringBootTest
 @Testcontainers
 @AutoConfigureMockMvc
-@Import({CommunitySpringbootMongodbSpringdataApp.class})
-@ContextConfiguration(initializers = MongoInitializer.class)
-class SuccessExecutionTest {
+@ExtendWith(SpringExtension.class)
+@Import({MongoDBSyncSuccessExecutionTest.TestConfiguration.class, CommunitySpringbootMongodbSyncApp.class})
+class MongoDBSyncSuccessExecutionTest {
+
+    @Container
+    public static final MongoDBContainer mongoDBContainer = new MongoDBContainer(DockerImageName.parse("mongo:4.0.10"));
 
     @Autowired
     private StartedEventListener startedEventListener;
@@ -36,6 +51,7 @@ class SuccessExecutionTest {
     private FailureEventListener failureEventListener;
 
 
+
     @Test
     @DisplayName("SHOULD execute all the expected changes WHEN executed IF happy path")
     void allExpectedChangeExecuted() {
@@ -45,11 +61,32 @@ class SuccessExecutionTest {
         assertEquals(CInsertAnotherDocument.class.getName(), ChangesTracker.get(2));
     }
 
+
+    @Test
+    @DisplayName("SHOULD execute all the expected changes WHEN executed IF happy path")
+    void profiledChangeNotExecuted() {
+        assertFalse(ChangesTracker.contains(ProfileNotIncludedChange.class.getName()));
+    }
+
+
+
     @Test
     @DisplayName("SHOULD trigger start and success event WHEN executed IF happy path")
     void events() {
         assertTrue(startedEventListener.executed);
         assertTrue(successEventListener.executed);
         assertFalse(failureEventListener.executed);
+    }
+
+    @Configuration
+    static class TestConfiguration {
+        @Bean
+        @Primary
+        public MongoClient mongoClient() {
+            return MongoClients.create(MongoClientSettings
+                    .builder()
+                    .applyConnectionString(new ConnectionString(mongoDBContainer.getConnectionString()))
+                    .build());
+        }
     }
 }
