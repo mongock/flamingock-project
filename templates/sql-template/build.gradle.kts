9f6256bfffac657f7d3817f13384daa4fed30e1f
Antonio Perez Dieppa <aperezdieppa@gmail.com>
2023-12-22 21:16:26 +0000

    removed unnecessary gradle dependencies

--- templates/sql-template/build.gradle.kts
+++ templates/sql-template/build.gradle.kts
@@ -1,10 +1,4 @@
 dependencies {
     implementation(project(":flamingock-springboot-v2-runner"))
     implementation(project(":flamingock-template"))
-    compileOnly("org.springframework.boot:spring-boot:2.7.12")
-    compileOnly("org.springframework.boot:spring-boot-autoconfigure:2.7.12")
-    annotationProcessor("org.springframework.boot:spring-boot-configuration-processor:2.7.12")
-
-    testImplementation("org.testcontainers:mongodb:1.18.3")
-    testImplementation("org.testcontainers:junit-jupiter:1.18.3")
 }
