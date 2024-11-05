c920c2fd6c79301ce52ff5d096580a9092f11554
Antonio Perez Dieppa <aperezdieppa@gmail.com>
2024-11-05 12:46:59 +0000

    minor refactor

--- flamingock-core-api/src/main/java/io/flamingock/core/api/FlamingockMetadata.java
+++ flamingock-core-api/src/main/java/io/flamingock/core/api/FlamingockMetadata.java
@@ -1,12 +1,18 @@
 package io.flamingock.core.api;
 
 
+import io.flamingock.commons.utils.JsonObjectMapper;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.Collection;
 
 public class FlamingockMetadata {
 
     public static final String FILE_PATH = "META-INF/flamingock-metadata.txt";
+
+    private static FlamingockMetadata instance;
 
     private boolean suppressedProxies;
 
@@ -15,22 +21,53 @@ public class FlamingockMetadata {
     public FlamingockMetadata() {
     }
 
-    public FlamingockMetadata(boolean suppressedProxies,
-                              Collection<String> classes) {
+    public FlamingockMetadata(boolean suppressedProxies, Collection<String> classes) {
         this.suppressedProxies = suppressedProxies;
         this.classes = classes;
+    }
+
+    public static synchronized FlamingockMetadata getInstance() {
+        if (instance == null) {
+            instance = loadFromFile();
+        }
+        return instance;
+    }
+
+    private static FlamingockMetadata loadFromFile() {
+        ClassLoader classLoader = FlamingockMetadata.class.getClassLoader();
+        try (InputStream inputStream = classLoader.getResourceAsStream("META-INF/flamingock-metadata.txt")) {
+            if (inputStream != null) {
+                StringBuilder sb = new StringBuilder();
+                try (BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream))) {
+                    String line;
+
+                    while ((line = reader.readLine()) != null) {
+                        sb.append(line);
+                    }
+                }
+
+                return JsonObjectMapper.DEFAULT_INSTANCE.readValue(sb.toString(), FlamingockMetadata.class);
+
+
+            } else {
+                throw new RuntimeException("annotated-classes.txt not found in META-INF");
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+
+        }
     }
 
     public boolean isSuppressedProxies() {
         return suppressedProxies;
     }
 
-    public Collection<String> getClasses() {
-        return classes;
-    }
-
     public void setSuppressedProxies(boolean suppressedProxies) {
         this.suppressedProxies = suppressedProxies;
+    }
+
+    public Collection<String> getClasses() {
+        return classes;
     }
 
     public void setClasses(Collection<String> classes) {
@@ -40,8 +77,6 @@ public class FlamingockMetadata {
 
     @Override
     public String toString() {
-        return "FlamingockMetadata{" + "suppressedProxies=" + suppressedProxies +
-                ", classes=" + classes +
-                '}';
+        return "FlamingockMetadata{" + "suppressedProxies=" + suppressedProxies + ", classes=" + classes + '}';
     }
 }
