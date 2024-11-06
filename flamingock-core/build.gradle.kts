90b67401b0441ead4e2d2644abad3e45bc3190af
Antonio Perez Dieppa <aperezdieppa@gmail.com>
2024-11-06 10:21:00 +0000

    remvoed commented imports

--- flamingock-core/build.gradle.kts
+++ flamingock-core/build.gradle.kts
@@ -28,7 +28,7 @@ dependencies {
     api("org.yaml:snakeyaml:2.2")
 
     api("org.apache.httpcomponents:httpclient:4.5.14")
-    //api("com.fasterxml.jackson.core:jackson-core:$jacksonVersion")
+    
     api("com.fasterxml.jackson.core:jackson-databind:$jacksonVersion")
 //    api("com.fasterxml.jackson.core:jackson-annotations:$jacksonVersion")
 
--- utils/build.gradle.kts
+++ utils/build.gradle.kts
@@ -24,9 +24,9 @@ dependencies {
     api("org.yaml:snakeyaml:2.2")
 //
     api("org.apache.httpcomponents:httpclient:4.5.14")
-    //api("com.fasterxml.jackson.core:jackson-core:$jacksonVersion")
+    
     api("com.fasterxml.jackson.core:jackson-databind:$jacksonVersion")
-    //api("com.fasterxml.jackson.core:jackson-annotations:$jacksonVersion")
+    
 
 //
 //    testImplementation(project(":commons:test-utils"))
