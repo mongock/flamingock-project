380b24bb4fcbdefbe66b94dc50d5decef416e746
Antonio Perez Dieppa <aperezdieppa@gmail.com>
2024-11-26 11:54:30 +0000

    fix: checking if artefact already

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,24 @@
 import org.jreleaser.model.Active
 
+import java.net.URI
+import java.net.http.HttpClient
+import java.net.http.HttpRequest
+import java.net.http.HttpResponse
+import java.util.Base64
+
+import org.json.JSONObject
+
+buildscript {
+    repositories {
+        mavenCentral()
+    }
+
+    dependencies {
+        classpath("org.json:json:20210307")
+    }
+}
+
+
 plugins {
     `kotlin-dsl`
     `maven-publish`
@@ -13,13 +32,14 @@ allprojects {
     version = "0.0.10-beta"
 
     apply(plugin = "org.jetbrains.kotlin.jvm")
+
 }
 
 val projectsToRelease = setOf(
-//    "flamingock-core",
-//    "flamingock-core-api",//required by flamingock-graalvm
-//    "flamingock-springboot-v2-runner",
-//    "flamingock-springboot-v3-runner",
+    "flamingock-core",
+    "flamingock-core-api",//required by flamingock-graalvm
+    "flamingock-springboot-v2-runner",
+    "flamingock-springboot-v3-runner",
 //    "metadata-generator",
     "couchbase-driver",
     "couchbase-springboot-v2-driver",
@@ -35,152 +55,159 @@ val projectsToRelease = setOf(
 )
 
 
+
+
 subprojects {
 
     apply(plugin = "java-library")
 
     if (project.shouldBeReleased()) {
+        if(project.notReleasedYet()) {
 
-        java {
-            withSourcesJar()
-            withJavadocJar()
-        }
+            java {
+                withSourcesJar()
+                withJavadocJar()
+            }
 
-        tasks.register("createStagingDeployFolder") {
-            group = "build"
-            description = "Creates the staging-deploy folder inside the build directory."
+            tasks.register("createStagingDeployFolder") {
+                group = "build"
+                description = "Creates the staging-deploy folder inside the build directory."
 
-            doLast {
-                val stagingDeployDir = layout.buildDirectory.dir("jreleaser").get().asFile
-                if (!stagingDeployDir.exists()) {
-                    stagingDeployDir.mkdirs()
-                    println("Created: $stagingDeployDir")
+                doLast {
+                    val stagingDeployDir = layout.buildDirectory.dir("jreleaser").get().asFile
+                    if (!stagingDeployDir.exists()) {
+                        stagingDeployDir.mkdirs()
+                        println("Created: $stagingDeployDir")
+                    }
                 }
             }
-        }
 
-        tasks.matching { it.name == "publish" }.configureEach {
-            finalizedBy("createStagingDeployFolder")
-        }
+            tasks.matching { it.name == "publish" }.configureEach {
+                finalizedBy("createStagingDeployFolder")
+            }
 
+            apply(plugin = "maven-publish")
+            apply(plugin = "org.jreleaser")
 
+            publishing {
+                publications {
+                    create<MavenPublication>("maven") {
+                        groupId = project.group.toString()
+                        artifactId = project.name
+                        version = project.version.toString()
 
-        apply(plugin = "maven-publish")
-        apply(plugin = "org.jreleaser")
-        publishing {
-            publications {
-                create<MavenPublication>("maven") {
-                    groupId = project.group.toString()
-                    artifactId = project.name
-                    version = project.version.toString()
+                        from(components["java"])
 
-                    from(components["java"])
-
-                    pom {
-                        name.set(project.name)
-                        description.set("Description should be here")
-                        url.set("https://github.com/mongock/flamingock-project")
-                        inceptionYear.set("2024")
-
-                        licenses {
-                            license {
-                                name.set("Apache-2.0")
-                                url.set("https://spdx.org/licenses/Apache-2.0.html")
-                            }
-                        }
-                        developers {
-                            developer {
-                                id.set("dieppa")
-                                name.set("Antonio Perez Dieppa")
-                            }
-                        }
-                        scm {
-                            connection.set("scm:git:https://github.com:mongock/flamingock-project.git")
-                            developerConnection.set("scm:git:ssh://github.com:mongock/flamingock-project.git")
+                        pom {
+                            name.set(project.name)
+                            description.set("Description should be here")
                             url.set("https://github.com/mongock/flamingock-project")
+                            inceptionYear.set("2024")
+
+                            licenses {
+                                license {
+                                    name.set("Apache-2.0")
+                                    url.set("https://spdx.org/licenses/Apache-2.0.html")
+                                }
+                            }
+                            developers {
+                                developer {
+                                    id.set("dieppa")
+                                    name.set("Antonio Perez Dieppa")
+                                }
+                            }
+                            scm {
+                                connection.set("scm:git:https://github.com:mongock/flamingock-project.git")
+                                developerConnection.set("scm:git:ssh://github.com:mongock/flamingock-project.git")
+                                url.set("https://github.com/mongock/flamingock-project")
+                            }
                         }
+                    }
+                }
+
+                repositories {
+                    maven {
+                        url = layout.buildDirectory.dir("staging-deploy").get().asFile.toURI()
                     }
                 }
             }
 
-            repositories {
-                maven {
-                    url = layout.buildDirectory.dir("staging-deploy").get().asFile.toURI()
+            jreleaser {
+                signing {
+                    active.set(Active.ALWAYS)
+                    armored = true
+                    enabled = true
+                    //Requires env variables
+                    // JRELEASER_GPG_PUBLIC_KEY
+                    // JRELEASER_GPG_SECRET_KEY
+                    // JRELEASER_GPG_PASSPHRASE
+
+                }
+
+                gitRootSearch.set(true)
+
+                release {
+
+                    github {
+                        //Requires env variable: JRELEASER_GITHUB_TOKEN
+                        overwrite.set(true)
+
+                        changelog {
+                            enabled.set(true)
+                            formatted.set(Active.ALWAYS)
+                            links.set(true)
+                            sort.set(org.jreleaser.model.Changelog.Sort.DESC)
+
+                            category {
+                                key.set("feat")
+                                title.set("🚀 New Features")
+                                labels.set(setOf("feat"))
+                                order.set(1)
+                            }
+                            category {
+                                key.set("fix")
+                                title.set("🐛 Bug Fixes")
+                                labels.set(setOf("fix"))
+                                order.set(2)
+                            }
+                            category {
+                                key.set("docs")
+                                title.set("📚 Documentation")
+                                labels.set(setOf("fix"))
+                                order.set(3)
+                            }
+                            category {
+                                key.set("chore")
+                                title.set("🛠️ Maintenance")
+                                labels.set(setOf("chore"))
+                                order.set(4)
+                            }
+                        }
+                    }
+                }
+
+                deploy {
+                    maven {
+                        mavenCentral {
+                            //Requires env variables
+                            // JRELEASER_MAVENCENTRAL_USERNAME
+                            // JRELEASER_MAVENCENTRAL_PASSWORD
+                            create("sonatype") {
+                                active.set(Active.ALWAYS)
+                                url.set("https://central.sonatype.com/api/v1/publisher")
+                                stagingRepository("build/staging-deploy")
+                            }
+
+
+                        }
+                    }
                 }
             }
+        } else {
+            println("$group:$name:$version ALREADY PUBLISHED")
         }
-
-        jreleaser {
-            signing {
-                active.set(Active.ALWAYS)
-                armored = true
-                enabled = true
-                //Requires env variables
-                // JRELEASER_GPG_PUBLIC_KEY
-                // JRELEASER_GPG_SECRET_KEY
-                // JRELEASER_GPG_PASSPHRASE
-
-            }
-
-            gitRootSearch.set(true)
-
-            release {
-
-                github {
-                    //Requires env variable: JRELEASER_GITHUB_TOKEN
-                    overwrite.set(true)
-
-                    changelog {
-                        enabled.set(true)
-                        formatted.set(Active.ALWAYS)
-                        links.set(true)
-                        sort.set(org.jreleaser.model.Changelog.Sort.DESC)
-
-                        category {
-                            key.set("feat")
-                            title.set("🚀 New Features")
-                            labels.set(setOf("feat"))
-                            order.set(1)
-                        }
-                        category {
-                            key.set("fix")
-                            title.set("🐛 Bug Fixes")
-                            labels.set(setOf("fix"))
-                            order.set(2)
-                        }
-                        category {
-                            key.set("docs")
-                            title.set("📚 Documentation")
-                            labels.set(setOf("fix"))
-                            order.set(3)
-                        }
-                        category {
-                            key.set("chore")
-                            title.set("🛠️ Maintenance")
-                            labels.set(setOf("chore"))
-                            order.set(4)
-                        }
-                    }
-                }
-            }
-
-            deploy {
-                maven {
-                    mavenCentral {
-                        //Requires env variables
-                        // JRELEASER_MAVENCENTRAL_USERNAME
-                        // JRELEASER_MAVENCENTRAL_PASSWORD
-                        create("sonatype") {
-                            active.set(Active.ALWAYS)
-                            url.set("https://central.sonatype.com/api/v1/publisher")
-                            stagingRepository("build/staging-deploy")
-                        }
-
-
-                    }
-                }
-            }
-        }
+    } else {
+        println("$group:$name:$version DOES NOT NEED PUBLISHING")
     }
 
 
@@ -234,3 +261,31 @@ subprojects {
 
 
 fun Project.shouldBeReleased() = projectsToRelease.contains(name)
+
+val client: HttpClient = HttpClient.newHttpClient()
+val encodedCredentials: String = Base64.getEncoder()
+    .encodeToString("${System.getenv("JRELEASER_MAVENCENTRAL_USERNAME")}:${System.getenv("JRELEASER_MAVENCENTRAL_PASSWORD")}".toByteArray())
+
+fun Project.notReleasedYet() : Boolean {
+    val request = HttpRequest.newBuilder()
+        .uri(URI.create("https://central.sonatype.com/api/v1/publisher/published?namespace=${group}&name=$name&version=$version"))
+        .header("accept", "application/json")
+        .header("Authorization", "Basic $encodedCredentials")
+        .GET()
+        .build()
+
+    val response = client.send(request, HttpResponse.BodyHandlers.ofString())
+
+    return if (response.statusCode() == 200) {
+        val jsonObject = JSONObject(response.body())
+        val map: Map<String, Any> = jsonObject.toMap()
+        if (map["published"] != null && map["published"] is Boolean) {
+            map["published"] as Boolean
+        } else {
+            false
+        }
+    } else {
+        //TODO implement retry
+        false
+    }
+}
