6ebfa47b9af2ab6e5fbaed2361d18225495f615e
Antonio Perez Dieppa <aperezdieppa@gmail.com>
2024-12-27 10:44:47 +0000

    wip

--- flamingock-core/src/main/java/io/flamingock/core/cloud/audit/CloudAuditWriter.java
+++ flamingock-core/src/main/java/io/flamingock/core/cloud/audit/CloudAuditWriter.java
@@ -10,10 +10,8 @@ import io.flamingock.core.engine.audit.writer.AuditEntryMapper;
 public interface CloudAuditWriter extends AuditWriter {
 
     default Result writeStartExecution(StartExecutionAuditItem auditItem) {
-        return Result.OK();//TODO remove this
-//        return writeEntry(AuditEntryMapper.map(auditItem));
+        return writeEntry(AuditEntryMapper.map(auditItem));
     }
-
 
     default Result writeExecution(ExecutionAuditItem auditItem) {
         return writeEntry(AuditEntryMapper.map(auditItem));
