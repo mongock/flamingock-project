7945739344d4d494255ded71424b983c587000e1
Rubén Berciano <bercianor@gmail.com>
2025-01-23 19:45:35 +0000

    add tests to mongodb transactioner

--- cloud-transactioners/mongodb-sync-v4-cloud-transactioner/build.gradle.kts
+++ cloud-transactioners/mongodb-sync-v4-cloud-transactioner/build.gradle.kts
@@ -4,6 +4,9 @@ dependencies {
 
     implementation("org.mongodb:mongodb-driver-sync:4.3.3")
 
+    testImplementation(project(":flamingock-core"))
+    testImplementation("org.testcontainers:mongodb:1.18.3")
+    testImplementation("org.testcontainers:junit-jupiter:1.18.3")
 
     testImplementation(project(":utils-test"))
 }
---
+++ cloud-transactioners/mongodb-sync-v4-cloud-transactioner/src/test/java/io.flamingock.cloud.transaction.mongodb.sync.v4/changes/unhappypath/UnhappyInsertClientsChange.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2023 Flamingock ("https://oss.flamingock.io")
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.flamingock.cloud.transaction.mongodb.sync.v4.changes.unhappypath;
+
+import com.mongodb.client.ClientSession;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import io.flamingock.core.api.annotations.ChangeUnit;
+import io.flamingock.core.api.annotations.Execution;
+import org.bson.Document;
+
+@ChangeUnit(id = "insert-clients", order = "2")
+public class UnhappyInsertClientsChange {
+
+    @Execution
+    public void execution(MongoDatabase mongoDatabase, ClientSession clientSession) {
+        MongoCollection<Document> collection = mongoDatabase.getCollection("clientCollection");
+        collection.insertOne(clientSession, new Document().append("name", "Should Have Been Rolled Back"));;
+        throw new RuntimeException("Intended exception");
+    }
+}
