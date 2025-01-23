9fbbe8adc7ed01c123fed380e9a310e5cd7716ae
Antonio Perez Dieppa <aperezdieppa@gmail.com>
2025-01-23 20:29:21 +0000

    fix: when retrieving a dependency, passing the type declare in the changeUnit instead of the dependecy's type

--- cloud-transactioners/sql-cloud-transactioner/src/test/java/io/flamingock/cloud/transaction/sql/changes/happypath/HappyInsertClientsChange.java
+++ cloud-transactioners/sql-cloud-transactioner/src/test/java/io/flamingock/cloud/transaction/sql/changes/happypath/HappyInsertClientsChange.java
@@ -28,7 +28,7 @@ import java.sql.SQLException;
 public class HappyInsertClientsChange {
 
     @Execution
-    public void execution(@NonLockGuarded Connection connection) throws SQLException {
+    public void execution(Connection connection) throws SQLException {
         try(PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO CLIENTS(id, name) values(?, ?)")) {
             preparedStatement.setInt(1, 1);
             preparedStatement.setString(2, "Robert Mccoy");
