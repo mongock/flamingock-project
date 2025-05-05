5a511a26492df635fe9410dbf49ae2464c935ff1
Antonio Perez Dieppa <aperezdieppa@gmail.com>
2025-05-05 16:33:30 +0100

    @Nullable

--- flamingock-core-api/src/main/java/io/flamingock/core/api/annotations/Nullable.java
+++ flamingock-core-api/src/main/java/io/flamingock/core/api/annotations/Nullable.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2025 Flamingock (https://oss.flamingock.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.flamingock.core.api.annotations;
 
 
@@ -10,16 +25,5 @@ import java.lang.annotation.Target;
 @Target({ElementType.PARAMETER})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Nullable {
-
-
-  /**
-   * <p>Indicates the grade of non-lock-guard applied to a method.
-   * Does not have any effect at class level.
-   * </p>
-   *
-   * @return value
-   */
-  NonLockGuardedType[] value() default {NonLockGuardedType.METHOD, NonLockGuardedType.RETURN};
-
 
 }
