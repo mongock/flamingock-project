bc79d6b797034c3fea077283046f8fd00c8ed2b2
Antonio Perez Dieppa <aperezdieppa@gmail.com>
2025-06-12 08:21:44 +0100

    added comments

--- core/flamingock-core/src/main/java/io/flamingock/internal/core/community/driver/OverridesDrivers.java
+++ core/flamingock-core/src/main/java/io/flamingock/internal/core/community/driver/OverridesDrivers.java
@@ -16,16 +16,41 @@
 
 package io.flamingock.internal.core.community.driver;
 
+import io.flamingock.internal.core.builder.Driver;
+
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-
+/**
+ * Indicates that a {@link Driver} implementation should take precedence over other Driver implementations
+ * in the classpath when multiple candidates are discovered via {@link java.util.ServiceLoader}.
+ * <p>
+ * This annotation is particularly useful in inheritance hierarchies where both parent and child classes
+ * implement the Driver interface. When Flamingock discovers multiple Driver implementations through
+ * ServiceLoader, it needs to determine which one to use. A Driver annotated with {@code @OverridesDrivers}
+ * will be selected over other implementations.
+ * <p>
+ * The {@code value} attribute must be used to explicitly specify which Driver implementations this
+ * Driver should override. At least one Driver class must be provided.
+ *
+ * @see java.util.ServiceLoader
+ */
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface OverridesDrivers {
 
-    Class<?>[] value() default {};
+    /**
+     * Specifies which Driver implementations this Driver should override.
+     * <p>
+     * This attribute must be provided with at least one Driver class to override.
+     * The annotation is used to explicitly declare which Driver implementations
+     * should be overridden by this implementation when multiple candidates are
+     * discovered via ServiceLoader.
+     *
+     * @return an array of Driver classes that this Driver implementation overrides
+     */
+    Class<? extends Driver<?>>[] value();
 
 }
