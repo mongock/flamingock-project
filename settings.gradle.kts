6061fcab206fb4f1823ebedfd29c2de69025aee9
Antonio Perez Dieppa <aperezdieppa@gmail.com>
2025-06-12 06:34:23 +0100

    removed flamingock-ce-mongodb-v3 and springdata v2

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -50,17 +50,9 @@ include("community:flamingock-ce-mongodb-sync")
 project(":community:flamingock-ce-mongodb-sync").name = "flamingock-ce-mongodb-sync"
 project(":community:flamingock-ce-mongodb-sync").projectDir = file("community/flamingock-ce-mongodb-sync")
 
-include("community:flamingock-ce-mongodb-v3")
-project(":community:flamingock-ce-mongodb-v3").name = "flamingock-ce-mongodb-v3"
-project(":community:flamingock-ce-mongodb-v3").projectDir = file("community/flamingock-ce-mongodb-v3")
-
 include("community:flamingock-ce-mongodb-springdata-v3")
 project(":community:flamingock-ce-mongodb-springdata-v3").name = "flamingock-ce-mongodb-springdata-v3"
 project(":community:flamingock-ce-mongodb-springdata-v3").projectDir = file("community/flamingock-ce-mongodb-springdata-v3")
-
-include("community:flamingock-ce-mongodb-springdata-v2")
-project(":community:flamingock-ce-mongodb-springdata-v2").name = "flamingock-ce-mongodb-springdata-v2"
-project(":community:flamingock-ce-mongodb-springdata-v2").projectDir = file("community/flamingock-ce-mongodb-springdata-v2")
 
 include("community:flamingock-ce-mongodb-springdata-v4")
 project(":community:flamingock-ce-mongodb-springdata-v4").name = "flamingock-ce-mongodb-springdata-v4"
