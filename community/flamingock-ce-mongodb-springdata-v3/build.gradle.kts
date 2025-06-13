c5d173319798e5cbff51589289fea5bd4adb2970
Antonio Perez Diepa <aperezdieppa@gmail.com>
2025-06-13 07:31:18 +0100

    Mongodb springdata versions (#221)

--- community/flamingock-ce-mongodb-springdata-v3/build.gradle.kts
+++ community/flamingock-ce-mongodb-springdata-v3/build.gradle.kts
@@ -1,29 +1,40 @@
+import org.gradle.kotlin.dsl.invoke
+
+val versions = mapOf(
+    "mongodb" to "[4.0.0, 4.7.0)",
+    "spring-data" to "[3.0.0, 4.0.0)",
+    "springboot" to "[2.0.0, 3.0.0)"
+)
 dependencies {
     implementation(project(":utils:mongodb-util"))
     implementation(project(":importers:mongodb-importer-sync-v4"))
     implementation(project(":core:flamingock-core"))
     api(project(":community:flamingock-ce-mongodb-sync"))
 
+    compileOnly("org.mongodb:mongodb-driver-sync:${versions["mongodb"]}")//this filters the broader range in flamingock-ce-mongodb-sync
+    compileOnly("org.springframework.data:spring-data-mongodb:${versions["spring-data"]}")
 
-    implementation("org.springframework.boot:spring-boot-starter-data-mongodb:[2.0.0,3.0.0)")
-    annotationProcessor("org.springframework.boot:spring-boot-configuration-processor:[2.0.0,3.0.0)")
+    compileOnly("org.springframework.boot:spring-boot-autoconfigure:${versions["springboot"]}")
+    annotationProcessor("org.springframework.boot:spring-boot-configuration-processor:${versions["springboot"]}")
 
-
-
-
-    testImplementation(project(":core:flamingock-core"))
+    /*********************************************************
+     * TEST
+     *********************************************************/
     testImplementation("org.testcontainers:mongodb:1.18.3")
     testImplementation("org.testcontainers:junit-jupiter:1.18.3")
 
-//    Mongock
     testImplementation("io.mongock:mongock-standalone:5.5.0")
     testImplementation("io.mongock:mongodb-sync-v4-driver:5.5.0")
 }
 
-description = "${project.name}'s description"
+description = "MongoDB Spring Data v3.x integration module for Flamingock Community Edition, providing seamless integration with Spring Data MongoDB 3.x applications. Compatible with MongoDB Driver Sync 4.0.0-4.6.x and Spring Boot 2.x. Compatible with JDK 8 and above."
 
 java {
     toolchain {
         languageVersion.set(JavaLanguageVersion.of(8))
     }
+}
+
+configurations.testImplementation {
+    extendsFrom(configurations.compileOnly.get())
 }
