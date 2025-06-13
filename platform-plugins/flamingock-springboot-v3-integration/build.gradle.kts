4d2793261ebb90bc511a7140759a7d1bbeda8aa0
Antonio Perez Dieppa <aperezdieppa@gmail.com>
2025-06-13 06:40:32 +0100

    springboot versions

--- platform-plugins/flamingock-springboot-v3-integration/build.gradle.kts
+++ platform-plugins/flamingock-springboot-v3-integration/build.gradle.kts
@@ -1,22 +1,3 @@
-//dependencies {
-//    api(project(":core:flamingock-core"))
-//    compileOnly("org.springframework.boot:spring-boot:3.1.3")
-//    compileOnly("org.springframework.boot:spring-boot-autoconfigure:3.1.3")
-//    annotationProcessor("org.springframework.boot:spring-boot-configuration-processor:3.1.3")
-//
-//    compileOnly("org.springframework:spring-context:6.+")
-//
-//    testImplementation("org.springframework:spring-context:6.+")
-//
-//}
-//
-//description = "${project.name}'s description"
-//
-//java {
-//    toolchain {
-//        languageVersion.set(JavaLanguageVersion.of(17))
-//    }
-//}
 
 val versions = mapOf(
     "spring" to "[6.0.0,7.0.0)",
@@ -29,11 +10,9 @@ dependencies {
     compileOnly("org.springframework.boot:spring-boot:${versions["springBoot"]}")
     compileOnly("org.springframework.boot:spring-boot-autoconfigure:${versions["springBoot"]}")
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor:${versions["springBoot"]}")
-
-    testImplementation("org.springframework:spring-context:5.+")
 }
 
-description = "Spring Boot v2.x integration module for Flamingock, providing seamless configuration and autoconfiguration capabilities for Spring-based applications"
+description = "Spring Boot v3.x integration module for Flamingock, providing seamless configuration and autoconfiguration capabilities for Spring-based applications. Compatible with JDK 17 and above."
 
 java {
     toolchain {
