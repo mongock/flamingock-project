a2bda0d3af7d13cfda833b8ff694fac29f306e4c
Antonio Perez Dieppa <aperezdieppa@gmail.com>
2025-07-09 06:42:25 +0100

    flamingock metadata file

--- core/flamingock-core-commons/src/main/java/io/flamingock/internal/common/core/metadata/Constants.java
+++ core/flamingock-core-commons/src/main/java/io/flamingock/internal/common/core/metadata/Constants.java
@@ -2,11 +2,9 @@ package io.flamingock.internal.common.core.metadata;
 
 public final class Constants {
 
-    public static final String TEMPLATED_PIPELINE_FILE_PATH = "META-INF/flamingock/templated-pipeline.json";
-    public static final String FULL_PIPELINE_FILE_PATH = "META-INF/flamingock/full-pipeline.json";
+    public static final String FULL_PIPELINE_FILE_PATH = "META-INF/flamingock/metadata.json";
 
-    public static final String TEMPLATED_GRAALVM_REFLECT_CLASSES_PATH = "META-INF/flamingock/templated-reflect-classes.txt";
-    public static final String FULL_GRAALVM_REFLECT_CLASSES_PATH = "META-INF/flamingock/full-reflect-classes.txt";
+    public static final String FULL_GRAALVM_REFLECT_CLASSES_PATH = "META-INF/flamingock/reflection-classes.txt";
 
 
     private Constants() {}
